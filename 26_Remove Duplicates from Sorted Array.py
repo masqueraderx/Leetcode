fc19610f5644bd23ad714bf337e562de2fa2c0dd
masqueraderx <973296319@qq.com>
2020-12-18 15:25:45 +0800

    26. Remove Duplicates from Sorted Array second commit

--- 26_Remove Duplicates from Sorted Array.py
+++ 26_Remove Duplicates from Sorted Array.py
@@ -6,12 +6,25 @@ Time: 12/18/2020
 
 class Solution:
     def removeDuplicates(self, nums):
-        j = 0
-        for i in range(1,len(nums)):
-            if nums[i] != nums[j]:
-                nums[j+1] = nums[i]
+        # j = 0
+        # for i in range(1,len(nums)):
+        #     if nums[i] != nums[j]:
+        #         nums[j+1] = nums[i]
+        #         j += 1
+        # return j+1
+
+        # 因为要求每个数最多重复1次。所以如果当前数据长度小于等于1，则不动，i指针往前走。
+        # 如果大于1的数组，当前的元素如果比其前面的第二个数大则一定可以加入。
+        # i: slow point; j: fast point
+        i, j = 0, 0
+        while j < len(nums):
+            if i < 1 or nums[j] > nums[i-1]:
+                nums[i] = nums[j]
+                i += 1
                 j += 1
-        return j+1
+            else:
+                j += 1
+        return i
 
 if __name__ == '__main__':
     s = Solution()
